8cdfe7c79a98e6f925aea7b827a1707d6bd3454e
mcm612 <michael.manguart@gmail.com>
2020-06-30 14:20:48 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
-import React from "react"
-
-export default function Home() {
-  return <div>Hello world!</div>
-}
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/Layout';
+import SimpleHero from '../components/SimpleHero';
+import Banner from '../components/Banner';
+import About from '../components/Home/About';
+import Services from '../components/Home/Services';
+export default () => (
+	<Layout>
+		<SimpleHero>
+			<Banner
+				title="the agency for what comes next"
+				info="We develop digital products that are properly balanced for feeling and function"
+			>
+				<Link to="/tours" className="btn-white">
+					Learn More
+				</Link>
+			</Banner>
+		</SimpleHero>
+		<About />
+		<Services />
+	</Layout>
+);
